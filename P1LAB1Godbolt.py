192382c883f50bf75d00d19a6bb2e5e910506294
godboltt5886 <godboltt5886@student.faytechcc.edu>
2024-10-03 18:31:41 +0000

    P1LAB1

---
+++ P1LAB1Godbolt.py
@@ -0,0 +1,13 @@
+#CTI 110
+#P1LAB1 - Receit
+# Godbolt
+#10/1/24
+
+
+first_name = input("Enter your first name: ")
+
+
+last_name = input("Enter your last name: ")
+print("Hello,  ",first_name,last_name)
+
+
